f96f92a17f55df201ba782ef0dddbf718c88e5c3
Alireza Riahi <ali.r.riahi@gmail.com>
2023-06-19 19:20:41 +0330

    add logger

--- apps/reservations/src/reservations.module.ts
+++ apps/reservations/src/reservations.module.ts
@@ -1,19 +1,19 @@
 import { Module } from '@nestjs/common';
 import { ReservationsService } from './reservations.service';
 import { ReservationsController } from './reservations.controller';
-import { DatabaseModule } from '@app/common';
+import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationRepository } from './reservation.repository';
 import {
   ReservationDocument,
   ReservationSchema,
 } from './models/reservation.schema';
-
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([
       { name: ReservationDocument.name, schema: ReservationSchema },
     ]),
+    LoggerModule,
   ],
   controllers: [ReservationsController],
   providers: [ReservationsService, ReservationRepository],
